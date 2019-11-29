9cc06a281056a37f04a8d80a2e0dc4481aa37e7a
Wesley Hendrikx <wesley.hendrikx@pxl.be>
2019-11-29 17:20:29 +0100

    improve tests on Converter of Lottery exercise on ADO.NET

--- Chapter10_ADO_Transactions/Exercise2/Lottery.Tests/DrawNumbersConverterTests.cs
+++ Chapter10_ADO_Transactions/Exercise2/Lottery.Tests/DrawNumbersConverterTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using Guts.Client.Classic;
 using Guts.Client.Shared;
@@ -33,7 +34,7 @@ Lottery.UI\Converters\DrawNumbersConverter.cs;")]
         public void _01_Convert_ShouldCreateACommaSeperatedStringOfNumbers()
         {
             //Arrange
-            var drawedNumbers = new List<DrawNumber>
+            var drawedNumbers = new HashSet<DrawNumber>
             {
                 new DrawNumberBuilder(_random).Build(),
                 new DrawNumberBuilder(_random).Build(),
@@ -41,36 +42,57 @@ Lottery.UI\Converters\DrawNumbersConverter.cs;")]
             };
 
             //Act
-            var result = _converter.Convert(drawedNumbers, null, null, null);
+            object result = null;
+            try
+            {
+                result = _converter.Convert(drawedNumbers, null, null, null);
+            }
+            catch (InvalidCastException)
+            {
+                Assert.Fail("An InvalidCastException is thrown when the value is a Hashset<DrawNumber>. " +
+                            "Cast to an IEnumerable<DrawNumber> to be able to handle all collection types.");
+            }
 
             //Assert
-            Assert.That(result, Is.Not.Null, () => "The converted object should not be null.");
+            Assert.That(result, Is.Not.Null, "The converted object should not be null.");
             Assert.That(result, Is.TypeOf<string>(),
-                () => $"The converted object should be a string, but was '{result.GetType().FullName}'.");
+                 $"The converted object should be a string, but was '{result.GetType().FullName}'.");
             var numberString = (string)result;
             Assert.That(drawedNumbers.All(d => numberString.Contains(d.Number.ToString())), Is.True,
-                () => "The converted string does not contain all the numbers.");
+                 "The converted string does not contain all the numbers.");
             Assert.That(numberString.Count(c => c == ','), Is.EqualTo(drawedNumbers.Count - 1),
-                () =>
-                    $"The converted string should contain {drawedNumbers.Count - 1} comma's when there are {drawedNumbers.Count} drawed numbers.");
+                $"The converted string should contain {drawedNumbers.Count - 1} comma's when there are {drawedNumbers.Count} drawed numbers.");
             Assert.That(numberString, Does.Not.EndsWith(" ").And.Not.EndsWith(","),
-                () => "The converted string should not end with a whitespace or a comma.");
+                 "The converted string should not end with a whitespace or a comma.");
         }
 
         [MonitoredTest("DrawNumbersConverter - Convert should sort the numbers by position"), Order(2)]
         public void _02_Convert_ShouldSortTheNumbersByPosition()
         {
             //Arrange
-            var drawedNumbers = new List<DrawNumber>
+            var drawNumber5 = new DrawNumberBuilder(_random).WithPosition(5).Build();
+            var drawNumber10 = new DrawNumberBuilder(_random).WithPosition(10).Build();
+            var drawNumber1 = new DrawNumberBuilder(_random).WithPosition(1).Build();
+            var drawNumberWithoutPosition = new DrawNumberBuilder(_random).Build();
+            var drawedNumbers = new Collection<DrawNumber>
             {
-                new DrawNumberBuilder(_random).WithPosition(5).Build(),
-                new DrawNumberBuilder(_random).WithPosition(10).Build(),
-                new DrawNumberBuilder(_random).WithPosition(1).Build(),
-                new DrawNumberBuilder(_random).Build()
+                drawNumber5,
+                drawNumber10,
+                drawNumber1,
+                drawNumberWithoutPosition
             };
 
             //Act
-            var result = _converter.Convert(drawedNumbers, null, null, null);
+            object result = null;
+            try
+            {
+                result = _converter.Convert(drawedNumbers, null, null, null);
+            }
+            catch (InvalidCastException)
+            {
+                Assert.Fail("An InvalidCastException is thrown when the value is a Collection<DrawNumber>. " +
+                            "Cast to an IEnumerable<DrawNumber> to be able to handle more collection types.");
+            }
 
             //Assert
             Assert.That(result, Is.Not.Null, () => "The converted object should not be null.");
@@ -85,16 +107,16 @@ Lottery.UI\Converters\DrawNumbersConverter.cs;")]
             }
             catch (Exception)
             {
-                Assert.Fail("The converted string is not a list of numbers seperated by a comma.");
+                Assert.Fail("The converted string is not a list of numbers separated by a comma.");
             }
 
             Assert.That(numbers, Has.Count.EqualTo(drawedNumbers.Count),
-                () => "The converted string does not contain all the numbers.");
-            Assert.That(numbers[0], Is.EqualTo(drawedNumbers[3].Number),
-                () => "Incorrect order. Numbers without a position should be first.");
-            Assert.That(numbers[1], Is.EqualTo(drawedNumbers[2].Number), () => "Incorrect order.");
-            Assert.That(numbers[2], Is.EqualTo(drawedNumbers[0].Number), () => "Incorrect order.");
-            Assert.That(numbers[3], Is.EqualTo(drawedNumbers[1].Number), () => "Incorrect order.");
+                "The converted string does not contain all the numbers.");
+            Assert.That(numbers[0], Is.EqualTo(drawNumberWithoutPosition.Number),
+                 "Incorrect order. Numbers without a position should be first.");
+            Assert.That(numbers[1], Is.EqualTo(drawNumber1.Number), "Incorrect order.");
+            Assert.That(numbers[2], Is.EqualTo(drawNumber5.Number), "Incorrect order.");
+            Assert.That(numbers[3], Is.EqualTo(drawNumber10.Number), "Incorrect order.");
         }
     }
 }
