84f7bee388f7426f7469b469c151dd427a644808
Wesley Hendrikx <wesley.hendrikx@pxl.be>
2020-09-21 15:40:54 +0200

    Review exercise 3 and 4 of chapter 3 (layout)

--- Chapter3_WPF_Layout/Exercise3.Tests/MainWindowTests.cs
+++ Chapter3_WPF_Layout/Exercise3.Tests/MainWindowTests.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -22,17 +18,14 @@ namespace Exercise3.Tests
     {
         private TestWindow<MainWindow> _window;
         private Canvas _canvas;
-        private IList<Ellipse> _ellipsesList;
+        private IList<Ellipse> _ellipses;
 
         [OneTimeSetUp]
         public void Setup()
         {
             _window = new TestWindow<MainWindow>();
-
             _canvas = _window.GetUIElements<Canvas>().FirstOrDefault();
-
-            _ellipsesList = _canvas.FindVisualChildren<Ellipse>().ToList();
-
+            _ellipses = _canvas.FindVisualChildren<Ellipse>().ToList();
         }
 
         [OneTimeTearDown]
@@ -46,34 +39,37 @@ namespace Exercise3.Tests
         {
             var codeBehindFilePath = @"Exercise3\MainWindow.xaml.cs";
             var fileHash = Solution.Current.GetFileHash(codeBehindFilePath);
-            Assert.That(fileHash, Is.EqualTo("90-4F-41-EA-49-EC-42-AD-F0-69-F1-ED-DE-4E-16-97"),
-                () =>
-                    $"The file '{codeBehindFilePath}' has changed. " +
-                    "Undo your changes on the file to make this test pass. " +
-                    "This exercise can be completed by purely working with XAML.");
+            Assert.That(fileHash, Is.EqualTo("6E-B4-E0-0D-58-AB-72-FB-4E-AA-DB-C1-7B-19-AA-F0"),
+                $"The file '{codeBehindFilePath}' has changed. " +
+                "Undo your changes on the file to make this test pass. " +
+                "This exercise can be completed by purely working with XAML.");
         }
 
-        [MonitoredTest("There have to be 5 ellipses"), Order(2)]
-        public void _02_ShouldHaveFiveEllipses()
+        [MonitoredTest("Should have 5 ellipses in a Canvas"), Order(2)]
+        public void _02_ShouldHaveFiveEllipsesInACanvas()
         {
-            Assert.That(_ellipsesList.Count, Is.EqualTo(5), "There have to be 5 Ellipses.");
+            Assert.That(_canvas, Is.Not.Null, "Cannot find a 'Canvas' in the window.");
+            Assert.That(_canvas.Parent, Is.SameAs(_window.Window), "The 'Canvas' should be the (only) child of the 'Window'.");
+            Assert.That(_ellipses.Count, Is.EqualTo(5), "There have to be 5 Ellipses.");
+            Assert.That(_ellipses.All(e => e.Parent == _canvas), Is.True, "The ellipses should be children of the 'Canvas'.");
         }
 
-        [MonitoredTest("There have to be 5 ellipses"), Order(2)]
+        [MonitoredTest("Should have 5 ellipses with the correct color and position"), Order(3)]
         public void _03_ShouldHave5EllipsesWithTheCorrectColorAndPosition()
         {
-            var blueEllipse = _ellipsesList.FirstOrDefault(e => e.Stroke == Brushes.Blue);
-            var blackEllipse = _ellipsesList.FirstOrDefault(e => e.Stroke == Brushes.Black);
-            var redEllipse = _ellipsesList.FirstOrDefault(e => e.Stroke == Brushes.Red);
-            var yellowEllipse = _ellipsesList.FirstOrDefault(e => e.Stroke == Brushes.Yellow);
-            var greenEllipse = _ellipsesList.FirstOrDefault(e => e.Stroke == Brushes.Green);
+            Ellipse blueEllipse = _ellipses.FirstOrDefault(e => e.Stroke == Brushes.Blue);
+            Assert.That(blueEllipse, Is.Not.Null, "Cannot find an ellipse with a 'Blue' stroke.");
+            Ellipse blackEllipse = _ellipses.FirstOrDefault(e => e.Stroke == Brushes.Black);
+            Assert.That(blackEllipse, Is.Not.Null, "Cannot find an ellipse with a 'Black' stroke.");
+            Ellipse redEllipse = _ellipses.FirstOrDefault(e => e.Stroke == Brushes.Red);
+            Assert.That(redEllipse, Is.Not.Null, "Cannot find an ellipse with a 'Red' stroke.");
+            Ellipse yellowEllipse = _ellipses.FirstOrDefault(e => e.Stroke == Brushes.Yellow);
+            Assert.That(yellowEllipse, Is.Not.Null, "Cannot find an ellipse with a 'Yellow' stroke.");
+            Ellipse greenEllipse = _ellipses.FirstOrDefault(e => e.Stroke == Brushes.Green);
+            Assert.That(greenEllipse, Is.Not.Null, "Cannot find an ellipse with a 'Green' stroke.");
 
-
-            Assert.That(blueEllipse.GetValue(Canvas.ZIndexProperty), Is.EqualTo(1), "The Blue Ellipse should be behind all the other ellipses.");
-            Assert.That(yellowEllipse.GetValue(Canvas.ZIndexProperty), Is.EqualTo(2), "The Yellow Ellipse should be between the Blue and the Black ellipse.");
-            Assert.That(blackEllipse.GetValue(Canvas.ZIndexProperty), Is.EqualTo(3), "The Black Ellipse should be between the Yellow and the Green ellipse.");
-            Assert.That(greenEllipse.GetValue(Canvas.ZIndexProperty), Is.EqualTo(4), "The Green Ellipse should be between the Black and the Red ellipse.");
-            Assert.That(redEllipse.GetValue(Canvas.ZIndexProperty), Is.EqualTo(5), "The Red Ellipse should be before all the other ellipses.");
+            Assert.That(_ellipses.All(e => e.Width == 150), Is.True, "Not all ellipses have a Width of 150 pixels.");
+            Assert.That(_ellipses.All(e => e.Height == 150), Is.True, "Not all ellipses have a Height of 150 pixels.");
 
             Assert.That(blueEllipse.GetValue(Canvas.TopProperty), Is.EqualTo(25), "The Blue Ellipse should be 25 pixels from the top.");
             Assert.That(blackEllipse.GetValue(Canvas.TopProperty), Is.EqualTo(25), "The Black Ellipse should be 25 pixels from the top.");
@@ -87,12 +83,35 @@ namespace Exercise3.Tests
             Assert.That(yellowEllipse.GetValue(Canvas.LeftProperty), Is.EqualTo(50), "The Yellow Ellipse should be 50 pixels from the left.");
             Assert.That(greenEllipse.GetValue(Canvas.LeftProperty), Is.EqualTo(125), "The Green Ellipse should be 125 pixels from the left.");
 
-            Assert.That(_ellipsesList.All(e => e.Width == 150), Is.True, "Not all ellipses have a Width of 150 pixels.");
-            Assert.That(_ellipsesList.All(e => e.Height == 150), Is.True, "Not all ellipses have a Height of 150 pixels.");
+            Assert.That(blueEllipse.IsBehind(yellowEllipse, blackEllipse, greenEllipse), Is.True,
+                "The blue ellipse should be behind yellow, black and green. " +
+                "Tip: make use of an attached property 'ZIndex' of 'Canvas'.");
 
+            Assert.That(yellowEllipse.IsBehind(blackEllipse, greenEllipse, redEllipse), Is.True,
+                "The yellow ellipse should be behind black, green and red. " +
+                "Tip: make use of an attached property 'ZIndex' of 'Canvas'.");
+
+            Assert.That(blackEllipse.IsBehind(greenEllipse, redEllipse), Is.True,
+                "The black ellipse should be behind green and red. " +
+                "Tip: make use of an attached property 'ZIndex' of 'Canvas'.");
+
+            Assert.That(greenEllipse.IsBehind(redEllipse), Is.True,
+                "The green ellipse should be behind red. " +
+                "Tip: make use of an attached property 'ZIndex' of 'Canvas'.");
         }
+    }
 
+    internal static class EllipseExtensions
+    {
+        public static bool IsBehind(this Ellipse ellipse, params Ellipse[] others)
+        {
+            int zIndex = (int)ellipse.GetValue(Canvas.ZIndexProperty);
 
-
+            return others.All(other =>
+            {
+                int otherZIndex = (int)other.GetValue(Canvas.ZIndexProperty);
+                return zIndex < otherZIndex;
+            });
+        }
     }
 }
--- Chapter3_WPF_Layout/Exercise4.Tests/MainWindowTests.cs
+++ Chapter3_WPF_Layout/Exercise4.Tests/MainWindowTests.cs
@@ -16,7 +16,7 @@ using NUnit.Framework;
 
 namespace Exercise4.Tests
 {
-    [ExerciseTestFixture("dotnet2", "H03", "Exercise04", @"Exercise4\MainWindow.xaml")]    
+    [ExerciseTestFixture("dotnet2", "H03", "Exercise04", @"Exercise4\MainWindow.xaml")]
     [Apartment(ApartmentState.STA)]
     public class MainWindowTests
     {
@@ -30,9 +30,7 @@ namespace Exercise4.Tests
         public void Setup()
         {
             _window = new TestWindow<MainWindow>();
-
             _grid = _window.GetUIElements<Grid>().FirstOrDefault();
-
             _labels = _window.GetUIElements<Label>().ToList();
             _gridSplitters = _window.GetUIElements<GridSplitter>().ToList();
         }
@@ -48,38 +46,104 @@ namespace Exercise4.Tests
         {
             var codeBehindFilePath = @"Exercise4\MainWindow.xaml.cs";
             var fileHash = Solution.Current.GetFileHash(codeBehindFilePath);
-            Assert.That(fileHash, Is.EqualTo("45-87-48-FC-AD-0E-8A-18-73-4F-C9-8C-27-6A-7A-D3"),
-                () =>
-                    $"The file '{codeBehindFilePath}' has changed. " +
-                    "Undo your changes on the file to make this test pass. " +
-                    "This exercise can be completed by purely working with XAML.");
+            Assert.That(fileHash, Is.EqualTo("90-4F-41-EA-49-EC-42-AD-F0-69-F1-ED-DE-4E-16-97"),
+                $"The file '{codeBehindFilePath}' has changed. " +
+                "Undo your changes on the file to make this test pass. " +
+                "This exercise can be completed by purely working with XAML.");
         }
 
-        [MonitoredTest("There have to columns participating In Size Sharing"), Order(2)]
-        public void _02_ShouldHaveTwoColumnsParticipatingInSizeSharing()
+
+        [MonitoredTest("Grid should have 5 columns"), Order(2)]
+        public void _02_GridShouldHaveFiveColumns()
         {
-            Assert.That(_grid.GetValue(Grid.IsSharedSizeScopeProperty), Is.True, "There have to be 5 Ellipses.");
+            Assert.That(_grid.ColumnDefinitions.Count, Is.EqualTo(5), "There have to be 5 columns (3 labels and 2 grid splitters).");
         }
 
-        [MonitoredTest("There have to columns participating In Size Sharing"), Order(3)]
-        public void _03_ShouldHaveFiveColumnsDefinitions()
+        [MonitoredTest("Should have 3 labels with the correct color"), Order(3)]
+        public void _03_ShouldHaveThreeLabelsWithTheCorrectColor()
         {
-            Assert.That(_grid.ColumnDefinitions.Count, Is.EqualTo(5), "There have to be 5 columns.");
+            Assert.That(_labels.Count, Is.EqualTo(3), () => "The grid must contain 3 labels");
+            Assert.That(_labels.All(l => l.Parent == _grid), Is.True, "All labels must be direct children of the grid");
+            Assert.That(_labels[0].Background == Brushes.Black, Is.True, "The first label should be black");
+            Assert.That(_labels[1].Background == Brushes.Yellow, Is.True, "The second label should be yellow");
+            Assert.That(_labels[2].Background == Brushes.Red, Is.True, "The third label should be red");
         }
 
-        [MonitoredTest("The first and the last column have to share the same Size group"), Order(4)]
-        public void _04_ShouldHaveTheFirstAndTheLastColumnInTheSameSizeGroup()
+        [MonitoredTest("Labels should be in the correct grid column"), Order(4)]
+        public void _04_ShouldHaveLabelsInTheCorrectGridColumn()
         {
-            Assert.That(_grid.ColumnDefinitions[0].SharedSizeGroup, Is.EqualTo(_grid.ColumnDefinitions[4].SharedSizeGroup), "There first and the last column have to share the samen Size Group.");
+            Assert.That(_labels.Count, Is.EqualTo(3), () => "The grid must contain 3 labels");
+            Assert.That(_labels[0].GetValue(Grid.ColumnProperty), Is.EqualTo(0), () => "The first label should be in the first column");
+            Assert.That(_labels[1].GetValue(Grid.ColumnProperty), Is.EqualTo(2), () => "The second label should be in the third column");
+            Assert.That(_labels[2].GetValue(Grid.ColumnProperty), Is.EqualTo(4), () => "The third label should be in the last column");
         }
-        [MonitoredTest("All the columns is given as much width as the elements within require, except for the middle (3rd) column" ), Order(5)]
-        public void _05_ShouldHaveAutomaticWidthColumnsExceptForTheMiddleColumn()
+
+        [MonitoredTest("There have to be 2 GridSplitters on the left and the right of the middle column"), Order(5)]
+        public void _05_ShouldHaveTwoGridSplittersLeftAndRightOfTheMiddleColumn()
         {
-            ColumnDefinitionCollection columns = _grid.ColumnDefinitions;
-            columns.RemoveAt(2);
+            Assert.That(_gridSplitters.Count, Is.EqualTo(2), () => "There should be two GridSplitters.");
+
+            Assert.That(_gridSplitters,
+                Has.One.Matches((GridSplitter splitter) => (int)splitter.GetValue(Grid.ColumnProperty) == 1),
+                () => "There should be one GridSplitter at the left of the middle column");
+
+            Assert.That(_gridSplitters,
+                Has.One.Matches((GridSplitter splitter) => (int)splitter.GetValue(Grid.ColumnProperty) == 3),
+                () => "There should be one GridSplitter at the right of the middle column");
+        }
+
+        [MonitoredTest("Should have horizontal and vertical alignment set correctly for the splitters"), Order(6)]
+        public void _06_ShouldHaveHorizontalAndVerticalAlignmentSetCorrectlyForTheSplitters()
+        {
+            Assert.That(_gridSplitters.Count, Is.EqualTo(2), () => "There should be two GridSplitters.");
+
+            Assert.That(_gridSplitters,
+                Has.All.Matches((GridSplitter splitter) => splitter.HorizontalAlignment == HorizontalAlignment.Center),
+                "Not all splitters are horizontally centered");
+
+            Assert.That(_gridSplitters,
+                Has.All.Matches((GridSplitter splitter) => splitter.VerticalAlignment == VerticalAlignment.Stretch),
+                "Not all splitters are vertically stretched");
+        }
+
+        [MonitoredTest("Should have the window as the scope for size sharing"), Order(7)]
+        public void _07_ShouldHaveTheWindowAsTheScopeForSizeSharing()
+        {
+            Assert.That(_window.Window.GetValue(Grid.IsSharedSizeScopeProperty), Is.True,
+                "The property 'IsSharedSizeScope' of 'Grid' must be true for 'Window'. " +
+                "See https://docs.microsoft.com/dotnet/api/system.windows.controls.grid.issharedsizescope");
+        }
+
+        [MonitoredTest("The first and the last column have to share the same size group"), Order(8)]
+        public void _08_ShouldHaveTheFirstAndTheLastColumnInTheSameSizeGroup()
+        {
+            _02_GridShouldHaveFiveColumns();
+
+            Assert.That(_grid.ColumnDefinitions.First().SharedSizeGroup, Is.Not.Null.Or.Empty,
+                "There first column should have a 'SharedSizeGroup' defined. " +
+                "See https://docs.microsoft.com/dotnet/desktop/wpf/controls/how-to-share-sizing-properties-between-grids?view=netframeworkdesktop-4.8");
+            Assert.That(_grid.ColumnDefinitions.Last().SharedSizeGroup, Is.Not.Null.Or.Empty,
+                "There last column should have a 'SharedSizeGroup' defined. " +
+                "See https://docs.microsoft.com/dotnet/desktop/wpf/controls/how-to-share-sizing-properties-between-grids?view=netframeworkdesktop-4.8");
+            Assert.That(_grid.ColumnDefinitions.First().SharedSizeGroup,
+                Is.EqualTo(_grid.ColumnDefinitions.Last().SharedSizeGroup),
+                "The first and the last column have to share the same Size Group.");
+        }
+
+        [MonitoredTest("Should have auto width colums except for the middle column"), Order(9)]
+        public void _09_ShouldHaveAutomWidthColumnsExceptForTheMiddleColumn()
+        {
+            _02_GridShouldHaveFiveColumns();
+
+            var columns = new List<ColumnDefinition>(_grid.ColumnDefinitions);
+            if (columns.Count > 2)
+            {
+                columns.RemoveAt(2);
+            }
+
             Assert.That(columns,
                 Has.All.Matches((ColumnDefinition column) => HasAutoColumnWidth(column)),
-                () => "All 'ColumnDefinition elements in the XAML should have auto Width. " +
+                () => "All (except the middle) 'ColumnDefinition' elements in the XAML should have auto Width. " +
                       "E.g. 'Width=\"auto\"'");
         }
 
@@ -87,64 +151,5 @@ namespace Exercise4.Tests
         {
             return columnDefinition.Width == GridLength.Auto;
         }
-
-        [MonitoredTest("The first and the last column have to share the same Size group"), Order(6)]
-        public void _6_ShouldHaveThreeLabels()
-        {
-            Assert.That(_labels.Count(), Is.EqualTo(3), () => "The grid must contain 3 labels");
-        }
-
-        [MonitoredTest("The first and the last column have to share the same Size group"), Order(7)]
-        public void _7_ShouldHaveThreeLabels()
-        {            
-            Assert.That(_labels.Count(), Is.EqualTo(3), () => "The grid must contain 3 labels");
-            Assert.That(_labels[0].Background == Brushes.Black, Is.True, () => "The first label should be black");
-            Assert.That(_labels[1].Background == Brushes.Yellow, Is.True, () => "The second label should be black");
-            Assert.That(_labels[2].Background == Brushes.Red, Is.True, () => "The third label should be black");
-        }
-
-        [MonitoredTest("Labels should be in the correct grid column"), Order(8)]
-        public void _8_ShouldHaveLabelsInTheCorrectGridColumn()
-        {
-            Assert.That(_labels[0].GetValue(Grid.ColumnProperty), Is.EqualTo(0), () => "The first label should be in the first column");
-            Assert.That(_labels[1].GetValue(Grid.ColumnProperty), Is.EqualTo(2), () => "The second label should be in the third column");
-            Assert.That(_labels[2].GetValue(Grid.ColumnProperty), Is.EqualTo(4), () => "The third label should be in the last column");
-        }
-
-        [MonitoredTest("Labels should be in the correct grid column"), Order(9)]
-        public void _9_ShouldHaveTwoGridSplitters()
-        {
-            Assert.That(_gridSplitters.Count, Is.EqualTo(2), () => "There should be two GridSplitters (Left and Right from the Yellow column).");
-        }
-
-        [MonitoredTest("There have to be Two GridSplitters on the Left and the Right of the middle column"), Order(9)]
-        public void _10_ShouldHaveTwoGridSplittersLeftAndRightFromTheMiddleColumn()
-        {
-            Assert.That(_gridSplitters,
-                Has.One.Matches((GridSplitter splitter) => (int)splitter.GetValue(Grid.ColumnProperty)==1), 
-                () => "There should be one GridSplitter at the left of column 1");
-            
-            Assert.That(_gridSplitters,
-                Has.One.Matches((GridSplitter splitter) => (int)splitter.GetValue(Grid.ColumnProperty) == 3),
-                () => "There should be one GridSplitter at the left of column 3");
-
-
-
-        }
-
-        [MonitoredTest("Horizontal Alignement of the GridSplitter Should be Center and Vertical Alignement Should be Stretch"), Order(10)]
-        public void _10_ShouldHaveHorizontalAndVerticalAlignementSetForTheGridSplitter()
-        {
-            Assert.That(_gridSplitters,
-                Has.All.Matches((GridSplitter splitter) => splitter.HorizontalAlignment == HorizontalAlignment.Center),
-                () => "There should be one GridSplitter at the left of column 1");
-
-            Assert.That(_gridSplitters,
-                Has.All.Matches((GridSplitter splitter) => splitter.VerticalAlignment == VerticalAlignment.Stretch),
-                () => "There should be one GridSplitter at the left of column 1");
-
-        }
-
-
     }
 }
