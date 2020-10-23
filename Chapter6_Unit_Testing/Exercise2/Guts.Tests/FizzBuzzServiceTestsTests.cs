897ee6d85a7d5bdc7dd91a06d51a5b8a6582a34d
Wesley Hendrikx <wesley.hendrikx@pxl.be>
2020-10-23 10:29:37 +0200

    update exercise 2 on unit tests

--- Chapter6_Unit_Testing/Exercise2/Guts.Tests/FizzBuzzServiceTestsTests.cs
+++ Chapter6_Unit_Testing/Exercise2/Guts.Tests/FizzBuzzServiceTestsTests.cs
@@ -15,7 +15,7 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Guts.Tests
 {
-    [ExerciseTestFixture("dotnet2", "H05", "Exercise02",
+    [ExerciseTestFixture("dotnet2", "H06", "Exercise02",
         @"FizzBuzz.Business\FizzBuzzService.cs;FizzBuzz.Business.Tests\FizzBuzzServiceTests.cs")]
     public class FizzBuzzServiceTestsTests
     {
