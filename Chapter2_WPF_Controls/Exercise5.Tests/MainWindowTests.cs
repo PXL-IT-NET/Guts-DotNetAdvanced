56bfabdaf191a127625cdf6164c27585b148e578
Wesley Hendrikx <wesley.hendrikx@pxl.be>
2021-09-15 12:23:04 +0200

    alter exercise 5 of chapter 2

--- Chapter2_WPF_Controls/Exercise5.Tests/MainWindowTests.cs
+++ Chapter2_WPF_Controls/Exercise5.Tests/MainWindowTests.cs
@@ -100,34 +100,32 @@ namespace Exercise5.Tests
             var stackPanel = countryItem.Header as StackPanel;
 
             Assert.That(stackPanel, Is.Not.Null,
-                () =>
-                    $"The country 'TreeViewItem' at position {positionInTree} should have a 'StackPanel' in its 'Header'.");
+                $"The country 'TreeViewItem' at position {positionInTree} should have a 'StackPanel' in its 'Header'.");
             Assert.That(stackPanel.Orientation, Is.EqualTo(Orientation.Horizontal),
-                () =>
-                    $"The country 'TreeViewItem' at position {positionInTree} should have a 'StackPanel' with a horizontal 'Orientation'.");
+                $"The country 'TreeViewItem' at position {positionInTree} should have a 'StackPanel' with a horizontal 'Orientation'.");
             Assert.That(stackPanel.Children, Has.Count.EqualTo(2),
-                () =>
-                    $"The country 'TreeViewItem' at position {positionInTree} should have a 'StackPanel' with 2 child controls.");
+                $"The country 'TreeViewItem' at position {positionInTree} should have a 'StackPanel' with 2 child controls.");
 
             Assert.That(countryItem.Margin.Top > 0 || stackPanel.Margin.Top > 0, Is.True,
-                () =>
-                    "There should be some space between the country items. " +
-                    "You can achieve this by adding some 'Margin' on the top of the 'TreeViewItem' or the 'StackPanel'. " +
-                    $"No top margin could by found for the country at position {positionInTree}.");
+                "There should be some space between the country items. " +
+                "You can achieve this by adding some 'Margin' on the top of the 'TreeViewItem' or the 'StackPanel'. " +
+                $"No top margin could by found for the country at position {positionInTree}.");
 
-            var image = stackPanel.Children[0] as Image;
-            Assert.That(image, Is.Not.Null,
-                () =>
-                    $"The 'StackPanel' of the country at position {positionInTree} should have an 'Image' control.");
-
-            var textBlock = stackPanel.Children[1] as TextBlock;
+            var textBlock = stackPanel.Children[0] as TextBlock;
             Assert.That(textBlock, Is.Not.Null,
-                () =>
-                    $"The 'StackPanel' of the country at position {positionInTree} should have an 'TextBlock' control.");
+                $"The 'StackPanel' of the country at position {positionInTree} should have a 'TextBlock' control as the first element.");
 
-            Assert.That(image.Margin.Right > 0 || textBlock.Margin.Left > 0, Is.True,
-                () =>
-                    $"There should be some space (Margin) between the 'Image' and the 'TextBlock' of the country at position {positionInTree}.");
+            var image = stackPanel.Children[1] as Image;
+            Assert.That(image, Is.Not.Null,
+                $"The 'StackPanel' of the country at position {positionInTree} should have an 'Image' control as the second element.");
+            Assert.That(image.Source, Is.Not.Null,
+                $"The 'Source' of the 'Image' of the country at position {positionInTree} should be set.");
+            Assert.That(image.Width > 0 || image.Height > 0, Is.True,
+                $"The width or height of the image of the country at position {positionInTree} must be set. " +
+                "Otherwise the image will be too big.");
+
+            Assert.That(textBlock.Margin.Right > 0 || image.Margin.Left > 0, Is.True,
+                $"There should be some space (Margin) between the 'TextBlock' and the 'Image' of the country at position {positionInTree}.");
         }
     }
 }
