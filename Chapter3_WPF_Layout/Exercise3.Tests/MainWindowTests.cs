7ee905a97224b4cb3aa92c208f7d9c2fee8b0fb3
Wesley Hendrikx <wesley.hendrikx@pxl.be>
2021-10-25 09:36:58 +0200

    improve tests on exercise 3 of chapter 3

--- Chapter3_WPF_Layout/Exercise3.Tests/MainWindowTests.cs
+++ Chapter3_WPF_Layout/Exercise3.Tests/MainWindowTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -71,66 +72,63 @@ namespace Exercise3.Tests
         [MonitoredTest("Should have 2 ellipses and 6 rectangles with the correct color and position"), Order(3)]
         public void _03_ShouldHave2EllipsesAnd6RectanglesWithTheCorrectColorAndPosition()
         {
+            IList<Ellipse> redEllipses = _ellipses.Where(e => e.Fill == Brushes.Red).OrderBy(e => e.GetValue(Canvas.TopProperty)).ToList();
+            Assert.That(redEllipses, Has.Count.EqualTo(2), "There should be 2 red ellipses");
+            IList<Rectangle> blueRectangles = _rectangles.Where(r => r.Fill == Brushes.Blue).OrderBy(e => e.GetValue(Canvas.TopProperty)).ToList();
+            Assert.That(blueRectangles, Has.Count.EqualTo(2), "There should be 2 blue rectangles");
+            IList<Rectangle> yellowRectangles = _rectangles.Where(r => r.Fill == Brushes.Yellow).OrderBy(e => e.GetValue(Canvas.TopProperty)).ToList();
+            Assert.That(yellowRectangles, Has.Count.EqualTo(2), "There should be 2 yellow rectangles");
+            IList<Rectangle> blackRectangles = _rectangles.Where(r => r.Fill == Brushes.Black).OrderBy(e => e.GetValue(Canvas.TopProperty)).ToList();
+            Assert.That(blackRectangles, Has.Count.EqualTo(2), "There should be 2 black rectangles");
 
-            Ellipse firstRedEllipse = _ellipses.FirstOrDefault(e => e.Fill == Brushes.Red && Convert.ToInt32(e.GetValue(Canvas.TopProperty)) == 80);
-            Assert.That(firstRedEllipse, Is.Not.Null, "Cannot find an ellipse with a 'Red' fill and a top margin o 80 pixels.");
-            Assert.That(firstRedEllipse.Width == 70, Is.True, "The first red ellipse does not have a Width of 70 pixels.");
-            Assert.That(firstRedEllipse.Height == 70, Is.True, "The first red ellipse does not have a Height of 70 pixels.");
+            AssertPosition(redEllipses.First(), "the first red ellipse", 80, 180);
+            Assert.That(redEllipses.First().Width == 70, Is.True, "The first red ellipse should have a Width of 70.");
+            Assert.That(redEllipses.First().Height == 70, Is.True, "The first red ellipse should have a Height of 70.");
 
-            Rectangle firstBlueRectangle = _rectangles.FirstOrDefault(e => e.Fill == Brushes.Blue && Convert.ToInt32(e.GetValue(Canvas.TopProperty)) == 100);
-            Assert.That(firstBlueRectangle, Is.Not.Null, "Cannot find a 'Blue' rectangle with a top margin of 100 pixels.");
+            AssertPosition(blueRectangles.First(), "the first blue rectangle", 100, 100);
+            AssertPosition(yellowRectangles.First(), "the first yellow rectangle", 150, 150);
+            AssertPosition(blackRectangles.First(), "the first black rectangle", 200, 200);
+            AssertPosition(blackRectangles.ElementAt(1), "the second black rectangle", 300, 200);
+            AssertPosition(redEllipses.ElementAt(1), "the second red ellipse", 250, 100);
+            Assert.That(redEllipses.ElementAt(1).Width == 150, Is.True, "The second red ellipse does not have a Width of 150 pixels.");
+            Assert.That(redEllipses.ElementAt(1).Height == 150, Is.True, "The second red ellipse does not have a Height of 150 pixels.");
 
-            Rectangle firstYellowRectangle = _rectangles.FirstOrDefault(e => e.Fill == Brushes.Yellow && Convert.ToInt32(e.GetValue(Canvas.TopProperty)) == 150);
-            Assert.That(firstYellowRectangle, Is.Not.Null, "Cannot find a 'Yellow' rectangle with a top margin of 150 pixels.");
-
-            Rectangle firstBlackRectangle = _rectangles.FirstOrDefault(e => e.Fill == Brushes.Black && Convert.ToInt32(e.GetValue(Canvas.TopProperty)) == 200);
-            Assert.That(firstBlueRectangle, Is.Not.Null, "Cannot find a 'Black' rectangle with a top margin of 200 pixels.");
-
-            Rectangle secondBlackRectangle = _rectangles.FirstOrDefault(e => e.Fill == Brushes.Black && Convert.ToInt32(e.GetValue(Canvas.TopProperty)) == 300);
-            Assert.That(secondBlackRectangle, Is.Not.Null, "Cannot find a 'Black' rectangle with a top margin of 300 pixels.");
-
-            Ellipse secondRedEllipse = _ellipses.FirstOrDefault(e => e.Fill == Brushes.Red && Convert.ToInt32(e.GetValue(Canvas.TopProperty)) == 250);
-            Assert.That(secondRedEllipse, Is.Not.Null, "Cannot find an ellipse with a 'Red' fill and a top margin of 250.");
-            Assert.That(secondRedEllipse.Width == 150, Is.True, "The second red ellipse does not have a Width of 150 pixels.");
-            Assert.That(secondRedEllipse.Height == 150, Is.True, "The second red ellipse does not have a Height of 150 pixels.");
-
-            Rectangle secondYellowRectangle = _rectangles.FirstOrDefault(e => e.Fill == Brushes.Yellow && Convert.ToInt32(e.GetValue(Canvas.TopProperty)) == 350);
-            Assert.That(secondYellowRectangle, Is.Not.Null, "Cannot find a 'Yellow' rectangle with a top margin of 350 pixels.");
-
-            Rectangle secondBlueRectangle = _rectangles.FirstOrDefault(e => e.Fill == Brushes.Blue && Convert.ToInt32(e.GetValue(Canvas.TopProperty)) == 400);
-            Assert.That(secondBlueRectangle, Is.Not.Null, "Cannot find a 'Blue' rectangle with a top margin of 400 pixels.");
+            AssertPosition(yellowRectangles.ElementAt(1), "the second yellow rectangle", 350, 150);
+            AssertPosition(blueRectangles.ElementAt(1), "the second blue rectangle", 400, 100);
 
             Assert.That(_rectangles.All(e => e.Height == 100), Is.True, "Not all rectangles have a Height of 100 pixels.");
             Assert.That(_rectangles.All(e => e.Width == 100), Is.True, "Not all rectangles have a Width of 100 pixels.");
-        
 
-            Assert.That(firstRedEllipse.GetValue(Canvas.LeftProperty), Is.EqualTo(180), "The first Red Ellipse should be 180 pixels from the left.");
-            Assert.That(firstBlueRectangle.GetValue(Canvas.LeftProperty), Is.EqualTo(100), "The first Blue Rectangle should be 100 pixels from the left.");
-            Assert.That(firstYellowRectangle.GetValue(Canvas.LeftProperty), Is.EqualTo(150), "The First Yellow Rectangle should be 150 pixels from the left.");
-            Assert.That(firstBlackRectangle.GetValue(Canvas.LeftProperty), Is.EqualTo(200), "The first Black Rectangle should be 200 pixels from the left.");
-            Assert.That(secondBlackRectangle.GetValue(Canvas.LeftProperty), Is.EqualTo(200), "The second Black Rectangle should be 200 pixels from the left.");
-            Assert.That(secondYellowRectangle.GetValue(Canvas.LeftProperty), Is.EqualTo(150), "The second Yellow Rectangle should be 150 pixels from the left.");
-            Assert.That(secondBlueRectangle.GetValue(Canvas.LeftProperty), Is.EqualTo(100), "The second Blue Rectangle should be 100 pixels from the left.");
-
-            Assert.That(firstBlueRectangle.IsBehindEllipse(firstRedEllipse), Is.True, 
+            Assert.That(blueRectangles.First().IsBehindEllipse(redEllipses.First()), Is.True,
                 "The first blue rectangle should be behind the first red ellipse." +
                 "Tip: make use of an attached property 'ZIndex' of 'Canvas'.");
-                
-            Assert.That(firstYellowRectangle.IsBehind(firstBlueRectangle, firstBlackRectangle), Is.True,
-                "The first yellow rectangle should be behind the first blue rectangle. " +
+
+            Assert.That(yellowRectangles.First().IsBehind(blueRectangles.First(), blackRectangles.First()), Is.True,
+                "The first yellow rectangle should be behind the first blue and first black rectangle. " +
                 "Tip: make use of an attached property 'ZIndex' of 'Canvas'.");
 
-            Assert.That(firstBlackRectangle.GetValue(Canvas.ZIndexProperty), Is.EqualTo(secondBlackRectangle.GetValue(Canvas.ZIndexProperty)),
+            Assert.That(blackRectangles.First().GetValue(Canvas.ZIndexProperty), Is.EqualTo(blackRectangles.ElementAt(1).GetValue(Canvas.ZIndexProperty)),
                 "The two black rectangles should not be behind each other. " +
                 "Tip: make use of an attached property 'ZIndex' of 'Canvas'.");
 
-            Assert.That(secondRedEllipse.IsBehindRectangle(firstBlackRectangle, secondBlackRectangle, secondYellowRectangle), Is.True,
-                "The second red ellipse should be behind the first black rectangle. " +
+            Assert.That(redEllipses.ElementAt(1).IsBehindRectangle(blackRectangles.First(), blackRectangles.ElementAt(1), yellowRectangles.ElementAt(1)), Is.True,
+                "The second red ellipse should not be behind the first black rectangle. " +
                 "Tip: make use of an attached property 'ZIndex' of 'Canvas'.");
 
-            Assert.That(secondBlueRectangle.IsBehind(secondYellowRectangle), Is.True,
+            Assert.That(blackRectangles.ElementAt(1).IsBehind(yellowRectangles.ElementAt(1)), Is.True,
                 "The second blue rectangle should be behind the second yellow rectangle. " +
                 "Tip: make use of an attached property 'ZIndex' of 'Canvas'.");
+        }
+
+        private void AssertPosition(FrameworkElement element, string elementDescription, int expectedTopMargin, int expectedLeftMargin)
+        {
+            Assert.That(element, Is.Not.Null, $"Cannot find {elementDescription}.");
+            double canvasTop = Convert.ToDouble(element.GetValue(Canvas.TopProperty));
+            Assert.That(canvasTop, Is.EqualTo(expectedTopMargin),
+                $"An attached property should be used to set the distance between {elementDescription} and the top of the Canvas to {expectedTopMargin}.");
+            double canvasLeft = Convert.ToDouble(element.GetValue(Canvas.LeftProperty));
+            Assert.That(canvasLeft, Is.EqualTo(expectedLeftMargin),
+                $"An attached property should be used to set the distance between {elementDescription} and the left of the Canvas to {expectedLeftMargin}.");
         }
     }
 
